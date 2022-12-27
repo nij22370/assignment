97ac43dd1b612082b24953e328c218a8b99b72c4
nij22370 <sandeshd531@gmail.com>
2022-12-27 14:30:36 +0545

    first commit

---
+++ refval.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int &ref=a;
+    cout<<a<<endl;
+    cout<<ref<<endl;
+     
+}
---
+++ sandesh.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello Sandesh";
+    return 0;
+}
